fdfe9148ecfdd792f9732086f238c99ec00fbe85
loverboy <loverboykosu@gmail.com>
2025-03-27 23:34:41 +0900

    feat: add Todo frame

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-
+import Todo from "./components/Todo.jsx";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>hello</h1>
+      <Todo />
     </>
   );
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,13 @@
+const Todo = () => {
+  return (
+    <>
+      <div>
+        <h1 className="bg-gray-900 text-white text-center p-4 text-lg ">
+          Todo app
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
