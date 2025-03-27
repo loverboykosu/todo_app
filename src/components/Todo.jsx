62f3050fd8f1dba778441c3f7f088f4e948d54f8
loverboy <loverboykosu@gmail.com>
2025-03-27 23:38:05 +0900

    feat: add taskform

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,6 +5,16 @@ const Todo = () => {
         <h1 className="bg-gray-900 text-white text-center p-4 text-lg ">
           Todo app
         </h1>
+
+        <p>Add task form</p>
+        <input
+          className="border-2 border-solid"
+          type="text"
+          placeholder="Add task form"
+        />
+        <button className="border-2 border-solid rounded bg-gray-700 text-white p-1">
+          Add
+        </button>
       </div>
     </>
   );
